26b12f12d3b7448b73ca2542756bd835165fad27
Matthias Kretschmann <m@kretschmann.io>
2015-09-12 23:11:22 +0200

    init, port codepen over

---
+++ README.md
@@ -0,0 +1,71 @@
+# App Store Badges
+
+> Pure CSS/SVG based App Store badges
+
+Demo | Blog post | [Codepen](http://codepen.io/kremalicious/details/EVVraP/)
+
+## Usage
+
+Install with npm:
+
+```bash
+npm i appstorebadges --save
+```
+
+Or install with Bower:
+
+```bash
+bower install appstorebadges --save
+```
+
+Or just link to the css file:
+
+```html
+<link rel="stylesheet" href="appstorebadges.min.css">
+```
+
+## Development
+
+First you need:
+
+- node & npm
+
+And install dependencies:
+
+```bash
+npm install
+```
+
+Then spin up livereloading dev server under http://localhost:1337
+
+```bash
+gulp
+```
+
+Or only compile new dist files:
+
+```bash
+gulp build
+```
+
+# The MIT License (MIT)
+
+Copyright (c) 2015 Matthias Kretschmann
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+'use strict';
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+// manually require modules that won"t get picked up by gulp-load-plugins
+var gulp = require('gulp'),
+    del = require('del'),
+    pkg = require('./package.json'),
+    assemble = require('assemble');
+
+// handle errors
+var onError = function(error) {
+    $.util.log($.util.colors.red('You fucked up:', error.message, 'on line' , error.lineNumber));
+    this.emit('end');
+}
+
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Config
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+var src      = 'src/',
+    dist     = 'dist/';
+
+
+// code banner
+var banner = [
+    '/**',
+    '**',
+    '** ------------------------------------------',
+    '**',
+    '**    <%= pkg.name %> v<%= pkg.version %>',
+    '**    <%= pkg.homepage %>',
+    '**    <%= pkg.license %> ',
+    '**',
+    '** ------------------------------------------',
+    '**',
+    '**/'
+].join('\n');
+
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Tasks
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+//
+// Delete build artifacts
+//
+gulp.task('clean', function() {
+    return del(dist + '**/*');
+});
+
+
+//
+// HTML
+//
+gulp.task('html', function() {
+    assemble.partials(src + 'partials/*.*');
+
+    return gulp.src(src + '*.hbs')
+        .pipe($.assemble(assemble))
+        .pipe($.extname())
+        .pipe(gulp.dest(dist))
+        .pipe($.connect.reload());
+});
+
+
+//
+// Styles
+//
+gulp.task('css', function() {
+    return gulp.src(src + 'styl/appstorebadges.styl')
+        .pipe($.stylus({ 'include css': true })).on('error', onError)
+        .pipe($.autoprefixer({ browsers: 'last 2 versions, ie 9' }))
+        .pipe($.cssmin())
+        .pipe($.header(banner, { pkg: pkg }))
+        .pipe($.rename({ suffix: '.min' }))
+        .pipe(gulp.dest(dist))
+        .pipe($.connect.reload());
+});
+
+
+//
+// Dev Server
+//
+gulp.task('connect', function() {
+    return $.connect.server({
+        root: [dist],
+        livereload: true,
+        port: 1337
+    });
+});
+
+
+//
+// Watch task
+//
+gulp.task('watch', function() {
+    gulp.watch([src + '**/*.styl'], ['css']);
+    gulp.watch([src + '**/*.{hbs,html}'], ['html']);
+});
+
+
+//
+// Dev Server
+//
+gulp.task('default', ['css', 'html', 'watch', 'connect']);
+
+
+//
+// Full build
+//
+gulp.task('build', ['css', 'html']);
